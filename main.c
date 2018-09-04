70b351e28528e4b9ad8b1cb15ff76c7e634a96e0
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2018-09-04 10:27:56 +0800

    initial commit

---
+++ main.c
@@ -0,0 +1,70 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+void printSystemMessage (
+	DWORD code,
+	DWORD langId,
+	const WCHAR* langDesc
+	)
+{
+	DWORD result;
+	WCHAR* message = NULL;
+
+	wprintf (L"FormatMessage (%d, %s) -> ", code, langDesc);
+
+	result = FormatMessageW (
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS |
+		FORMAT_MESSAGE_MAX_WIDTH_MASK, // no line breaks please
+		NULL,
+		code,
+		langId,
+		(LPWSTR) &message,
+		0,
+		NULL
+		);
+
+	if (result)
+		wprintf (L"%s\n", message);
+	else
+		wprintf (L"failed (%d)\n", GetLastError ());
+}
+
+int main ()
+{
+	static const DWORD codeTable [] =
+	{
+		ERROR_FILE_NOT_FOUND,
+		ERROR_ACCESS_DENIED,
+		ERROR_NOACCESS,
+		// ...
+	};
+
+	static const struct
+	{
+		DWORD m_id;
+		const WCHAR* m_desc;
+	}
+	langTable [] =
+	{
+		{ MAKELANGID (LANG_NEUTRAL, SUBLANG_NEUTRAL),     L"LANG_NEUTRAL, SUBLANG_NEUTRAL" },
+		{ MAKELANGID (LANG_NEUTRAL, SUBLANG_DEFAULT),     L"LANG_NEUTRAL, SUBLANG_DEFAULT" },
+		{ MAKELANGID (LANG_NEUTRAL, SUBLANG_SYS_DEFAULT), L"LANG_NEUTRAL, SUBLANG_SYS_DEFAULT" },
+		{ MAKELANGID (LANG_INVARIANT, SUBLANG_NEUTRAL),   L"LANG_INVARIANT, SUBLANG_NEUTRAL" },
+		{ MAKELANGID (LANG_ENGLISH, SUBLANG_DEFAULT),     L"LANG_ENGLISH, SUBLANG_DEFAULT" },
+		{ MAKELANGID (LANG_ENGLISH, SUBLANG_ENGLISH_US),  L"LANG_NEUTRAL, SUBLANG_ENGLISH_US" },
+	};
+
+	UINT i, j;
+
+	for (i = 0; i < _countof (codeTable); i++)
+	{
+		for (j = 0; j < _countof (langTable); j++)
+			printSystemMessage (codeTable [i], langTable [j].m_id, langTable [j].m_desc);
+
+		wprintf (L"\n");
+	}
+
+	return 0;
+}
